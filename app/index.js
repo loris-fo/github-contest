546063f2d1d9ee524c9d04028935f6506ee7081f
lorisfo <loris-fo@outlook.com>
2016-03-03 13:58:51 +0000

    Created react component with img, username and name of a user

--- app/index.js
+++ app/index.js
@@ -1,17 +1,25 @@
+var USER_DATA = {
+  image: "https://avatars1.githubusercontent.com/u/10195963?v=3&s=460",
+  username: "loris-fo",
+  nameUser: "loris"
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfileContainer = React.createClass({
   render: function() {
     return (
       <div>
-        <h3>Hello, World!</h3>
+        <img src={this.props.user.image} style={{width:100, height:100}} />
+        <a href={"https://www.github.com/" + this.props.user.username}>{this.props.user.username}</a>
+        <p>{this.props.user.nameUser}</p>
       </div>
     )
   }
 });
 
 ReactDOM.render(
-  <HelloWorld />,
+  <ProfileContainer user={USER_DATA} />,
   document.getElementById('app')
 )
