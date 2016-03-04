b8d8276b3c57f1a78227bbb90c8d83fc9170ce54
lorisfo <loris-fo@outlook.com>
2016-03-04 11:30:45 +0000

    Added routing to the app

---
+++ app/config/routes.js
@@ -0,0 +1,17 @@
+var React = require('react');
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var IndexRouter = ReactRouter.IndexRouter;
+var hashHistory = ReactRouter.hashHistory;
+
+var Main = require('../components/Main');
+var Home = require('../components/Home');
+
+var routes = (
+  <Router history={hashHistory}>
+    <Route path="/" component={Main}>
+      <IndexRouter component={Home} />
+    </Route>
+  </Router>
+)
